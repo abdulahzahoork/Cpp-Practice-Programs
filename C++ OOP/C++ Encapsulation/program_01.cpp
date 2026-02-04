321a1cbb28c79f5af2e73c2ca65a8b6b7d45d626
Muhammad Abdullah Khan <abdulahzahoork@gmail.com>
2026-02-04 23:47:59 +0500

    Practice 1 Problem

---
+++ C++ OOP/C++ Encapsulation/program_01.cpp
@@ -0,0 +1,38 @@
+// Problem: Student Class
+
+// Create a class Student with:
+// - Attributes: name, rollNo
+
+// A default constructor that assigns:
+// - name = "Unknown"
+// - rollNo = 0
+
+// A function display() to show data
+
+// Create an object in main() and display its values.
+
+#include <iostream>
+using namespace std;
+
+class Student {
+
+private:
+    string name;
+    int rollNo;
+
+public:
+    // ------ Default Constructor ------
+    Student() {
+        name = "Unknown";
+        rollNo = 0;
+    }
+
+    void display() const {
+        cout << "Name: " << name << "\nRoll no: " << rollNo << endl;
+    }
+};
+
+int main() {
+    Student s;
+    s.display();
+}
