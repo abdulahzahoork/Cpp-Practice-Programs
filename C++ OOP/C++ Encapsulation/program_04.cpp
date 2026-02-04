40e79af06e3081b1445707661a6d3d677fc4ba3a
Muhammad Abdullah Khan <abdulahzahoork@gmail.com>
2026-02-05 00:07:43 +0500

    Practice 1 Program

---
+++ C++ OOP/C++ Encapsulation/program_04.cpp
@@ -0,0 +1,39 @@
+// Problem: Bank Account
+
+// Create a class BankAccount with:
+// - Attributes: accountNumber, balance
+
+// A default constructor that:
+// - Sets account number to 0000
+// - Sets balance to 0
+
+// Function displayAccount()
+
+// Observe what happens when no values are passed
+
+#include <iostream>
+using namespace std;
+
+class BankAccount {
+
+private:
+    string accountNumber;
+    double balance;
+
+public:
+    BankAccount() {
+        accountNumber = "0000";
+        balance = 0;
+    }
+
+    void displayAccount() const {
+        cout << "Account Number: " << accountNumber << "\nBalance: " << balance << endl;
+    }
+};
+
+int main() {
+    BankAccount acc;
+    acc.displayAccount();
+
+    return 0;
+}
