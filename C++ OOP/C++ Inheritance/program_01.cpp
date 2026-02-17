509bfdc25146daea52e05f62f7d6be4dfcfc7e4d
Muhammad Abdullah Khan <abdulahzahoork@gmail.com>
2026-02-17 06:34:22 +0500

    Update Inheritance: program_01

--- C++ OOP/C++ Inheritance/program_01.cpp
+++ C++ OOP/C++ Inheritance/program_01.cpp
@@ -13,8 +13,11 @@
 // Create an object of Student and display all details.
 
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
+// ----------------- Base Class (Person) -----------------
 class Person {
 
 private: 
@@ -22,31 +25,64 @@ private:
     int age;
 
 public: 
-    Person(string n, int a): name(n), age(a) {}
+    Person (string n, int a): name(n), age((a<0)? 0 : a) {}
 
-    void displayPerson() const {
+    void display() const {
         cout << "Name: " << name << endl;
         cout << "Age: " << age << endl;
     }
 };
 
+// ----------------- Derived Class (Student) -----------------
 class Student : public Person {
 
 private: 
     int rollNo;
 
 public: 
-    Student (string n, int a, int r): Person(n, a), rollNo(r) {}
+    Student(string n, int a, int r): Person(n, a), rollNo(r) {}
 
     void displayStudent() const {
-        displayPerson();
-        cout << "Roll No: " << rollNo << endl;
+        display();
+        cout << "Roll no: " << rollNo << endl;
     }
 };
 
+// ----------------- Main Function -----------------
 int main() {
-    Student s1 ("Abdullah", 22, 27);
-    s1.displayStudent();
+
+    int n;
+    cout << "Enter number of students: ";
+    cin >> n;
+    cin.ignore();
+
+    string name;
+    int age, rollNo;
+
+    vector<Student> students;
+    students.reserve(n);
+
+    for (int i=0; i<n; i++) {
+        cout << "\nEnter details of Student no. " << i+1 << ": " << endl;
+
+        cout << "Enter name: ";
+        getline(cin, name);
+
+        cout << "Enter age: ";
+        cin >> age;
+
+        cout << "Enter roll no: ";
+        cin >> rollNo;
+        cin.ignore();
+
+        students.emplace_back(name, age, rollNo);
+    }
+
+    cout << "\n------------------ Students Details ------------------" << endl;
+    for (int i=0; i<n; i++) {
+        cout << "\nStudent no. " << i+1 << endl;
+        students[i].displayStudent();
+    }
 
     return 0;
 }
